c4a933380a3052307a44a4b0766a2b699f9fe509
David Grant <mrdavidgrant@outlook.com>
2017-10-24 17:40:45 -0400

    fixed datetime issue

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
 
   def update
     @activity = Session.find params[:id]
-    @activity[:completed_at] = Date.now
+    @activity[:completed_at] = DateTime.now
     if params[:liftsets].exists?
       @update = params[:liftsets]
       @update.each do |liftset|
