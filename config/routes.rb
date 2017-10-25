b788cecdc3a42ca79aa7f20891d1db44131871e7
David Grant <mrdavidgrant@outlook.com>
2017-10-25 11:39:21 -0400

    added route to show user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions, only: [:index, :create]
+      resources :users, only: [:show]
       resources :users, param: :user_id, only: [:show] do
         resources :activities
         resources :liftsets, only: [:destroy]
