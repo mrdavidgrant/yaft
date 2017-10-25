e4a9f1f9d8323ef4fc21a53bc85eaa00f857f010
David Grant <mrdavidgrant@outlook.com>
2017-10-25 10:12:29 -0400

    turned off authentication for testing/dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_request
+  # before_action :authenticate_request
   respond_to :json
   attr_reader :current_user
 
