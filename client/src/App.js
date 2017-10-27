45083cc047f1e7c850d86c61ba801a30a9f659ee
David Grant <mrdavidgrant@outlook.com>
2017-10-27 16:38:17 -0400

    fixed merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
 import logo from './appStyles/logo.svg';
 import './appStyles/App.css';
 import NewWorkout from './components/NewWorkout';
 
->>>>>>> feature/heroku
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-=======
 
       <div className="App">
 
@@ -33,7 +17,6 @@ class App extends Component {
         <NewWorkout />
       </div>
 
->>>>>>> feature/heroku
     );
   }
 }
