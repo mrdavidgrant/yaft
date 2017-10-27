499545b7b8506709250c2d6346e605d625e4a100
David Grant <mrdavidgrant@outlook.com>
2017-10-27 11:41:51 -0400

    fixed time issue in activities controller

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -62,7 +62,7 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
   end
 
   def post_params
-    params.require(:session).permit(:name, :user_id, :completed_at, :liftsets => [:session_id, :equipment, :rest, :reps, :weight, :motion])
+    params.require(:session).permit(:name, :user_id, :completed_at, :started_at, :liftsets => [:session_id, :equipment, :rest, :reps, :weight, :motion])
   end
 
 end
