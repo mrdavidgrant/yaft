543f014c230321e559cb7c30057286542751f84a
David Grant <mrdavidgrant@outlook.com>
2017-11-01 10:43:37 -0400

    testing heroku

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -63,7 +63,7 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
   end
 
   def post_params
-    params.require(:session).permit(:name, :user_id, :completed_at, :started_at)
+    params.permit(:session => {:name, :user_id, :completed_at, :started_at})
     params.permit(:liftsets => [:session_id, :started, :stopped, :rest, :reps, :weight, :motion_id, :equipment_id])
   end
 
